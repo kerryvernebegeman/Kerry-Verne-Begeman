58f5f84f7c324b08cc03f5c0516793f9eaeb182d
kerryvernebegeman <77469810+kerryvernebegeman@users.noreply.github.com>
2021-01-14 19:36:34 -0600

    Add files via upload

--- runway_model.py
+++ runway_model.py
@@ -1,45 +1,47 @@
-# MIT License
+import pickle
+import numpy as np
+import tensorflow as tf
+import dnnlib.tflib as tflib
+import runway
 
-# Copyright (c) 2019 Runway AI, Inc
+fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+@runway.setup(options={'checkpoint': runway.file(extension='.pkl')})
+def setup(opts):
+    global Gs
+    print('init')
+    tflib.init_tf()
+    print('reading pickle file')
+    with open(opts['checkpoint'], 'rb') as file:
+        _G, _D, Gs = pickle.load(file, encoding='latin1')
+    print('Create noise')
+    noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+    rnd = np.random.RandomState()
+    tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars})
+    return Gs
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+generate_inputs = {
+    'z': runway.vector(512, sampling_std=0.5),
+    'label': runway.number(min=0, max=100000, default=0, step=1), # generate random labels
+    'scale': runway.number(min=-2, max=2, default=0, step=0.05),  # magnitude of labels - 0 = no labels
+    'truncation': runway.number(min=0, max=1, default=1, step=0.1)
+}
 
-import random
-from PIL import Image
+@runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
+def convert(model, inputs):
+    z = inputs['z']
+    print(z)
+    label = int(inputs['label'])
+    scale = inputs['scale']
+    truncation = inputs['truncation']
+    latents = z.reshape((1, 512))
+    labels = scale * np.random.RandomState(label).randn(167)
+    labels = labels.reshape((1,167)).astype(np.float32)
+    images = model.run(latents, labels, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
+    output = np.clip(images[0], 0, 255).astype(np.uint8)
+    return {'image': output}
 
-class ExampleModel():
 
-    def __init__(self, options):
-        random.seed(options['seed'])
-        self.truncation = options['truncation']
-
-    # Generate an image based on some text.
-    def run_on_input(self, caption_text):
-
-        # This is an example of how you could use some input from
-        # @runway.setup(), like options['truncation'], later inside a
-        # function called by @runway.command().
-        text = caption_text[0:self.truncation]
-
-        # Return a red image if the input text is "red",
-        # otherwise return a blue image.
-        if text == 'red':
-            return Image.new('RGB', (512, 512), color = 'red')
-        else:
-            return Image.new('RGB', (512, 512), color = 'blue')
+if __name__ == '__main__':
+    runway.run()
